0792f69038ebfa2b84f91b0de6ce6ed28d8d7043
fabiodplacerda <lacerda96@gmail.com>
2023-12-11 14:12:27 +0000

    add articles page

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { getArticles } from "../utils/utils";
+import ArticleCard from "./ArticleCard";
 
 const Articles = () => {
   const [articles, SetArticles] = useState([]);
@@ -10,7 +11,18 @@ const Articles = () => {
     });
   });
 
-  return <h2>Articles</h2>;
+  return (
+    <>
+      <div id="articles">
+        <h2>Articles</h2>
+        <ul id="articles-list">
+          {articles.map((article) => {
+            return <ArticleCard key={article.article_id} article={article} />;
+          })}
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default Articles;
