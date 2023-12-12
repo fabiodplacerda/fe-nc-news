b1a9b1a3249805be001c5afee85099349a96d14b
fabiodplacerda <lacerda96@gmail.com>
2023-12-12 15:18:15 +0000

    added Loading component

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,20 +1,31 @@
-import { useEffect, useState } from "react";
-import { getCommentsByArticleId } from "../utils/utils";
+import { useEffect, useState } from 'react';
+import { getCommentsByArticleId } from '../utils/utils';
 
-import Comment from "./Comment";
+import Comment from './Comment';
+import Loading from './Loading';
 
 const Comments = ({ article_id }) => {
   const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    getCommentsByArticleId(article_id).then((data) => {
+    getCommentsByArticleId(article_id).then(data => {
       setComments(data.comments);
+      setIsLoading(false);
     });
   }, []);
 
+  if (isLoading) {
+    return <Loading dynamicText={'comments'} />;
+  }
+
+  if (!comments.length) {
+    return <h3>No comments yet. Be the first to comment!</h3>;
+  }
+
   return (
     <ul>
-      {comments.map((comment) => {
+      {comments.map(comment => {
         return <Comment key={comment.comment_id} comment={comment} />;
       })}
     </ul>
