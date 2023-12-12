3e236fd0f8f21e0da53c7867c1932465d5412f54
fabiodplacerda <lacerda96@gmail.com>
2023-12-12 15:16:14 +0000

    change div to li to be more semantic

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,15 +1,15 @@
-import "./Comment.css";
+import './Comment.css';
 
 const Comment = ({ comment }) => {
   return (
-    <div className="comment-container" key={comment.comment_id}>
+    <li className="comment-container" key={comment.comment_id}>
       <div className="comment-header">
         <h4 className="comment-author">@{comment.author}</h4>
         <p className="comment-date">{comment.created_at}</p>
       </div>
       <p className="comment-body">{comment.body}</p>
       <p className="comment-votes">Votes: {comment.votes}</p>
-    </div>
+    </li>
   );
 };
 
