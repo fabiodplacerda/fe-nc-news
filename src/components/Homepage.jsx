264046b816cd145671b356ac66de35047d27f854
fabiodplacerda <lacerda96@gmail.com>
2023-12-13 16:26:26 +0000

    added option to login with valid users

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -38,15 +38,23 @@ const Homepage = () => {
       <h2>Welcome{user !== '' ? ` ${user}` : ''}!</h2>
       <form onSubmit={submitHandler}>
         <label htmlFor="username">Username</label>
-        <input
-          type="text"
+
+        <select
           name="username"
           id="username"
-          placeholder="your username"
-          onChange={inputHandler}
           value={input}
-        />
-        <p>{errorMessage ? "user doesn't exist" : null}</p>
+          onChange={inputHandler}
+        >
+          <option value="null">---</option>
+          <option value="tickle122">tickle122</option>
+          <option value="grumpy19">grumpy19</option>
+          <option value="happyamy2016">happyamy2016</option>
+          <option value="cooljmessy">cooljmessy</option>
+          <option value="weegembump">weegembump</option>
+          <option value="jessjelly">jessjelly</option>
+        </select>
+
+        <p>{errorMessage ? 'Please Select one of the valid users' : null}</p>
         <button>Login</button>
       </form>
     </>
