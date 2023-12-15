15559331a18f7fec2ae0dab11d955f85eff1afc2
fabiodplacerda <lacerda96@gmail.com>
2023-12-15 12:41:53 +0000

    added some styles to loading screen

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,5 +1,23 @@
 const Loading = ({ dynamicText }) => {
-  return <p>Loading {dynamicText}...</p>;
+  return (
+    <div className="loading">
+      <div className="lds-spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <h2 id="loading-text">Loading {dynamicText}, please wait...</h2>
+    </div>
+  );
 };
 
 export default Loading;
